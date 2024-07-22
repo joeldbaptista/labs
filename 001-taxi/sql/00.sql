68ba0f4bc2bdbfd78b8c42c6965f6f5933b7f668
joel <jdbaptista@pm.me>
2024-07-22 17:02:01 +0100

    add current timestamp to staging

--- 001-taxi/sql/00.sql
+++ 001-taxi/sql/00.sql
@@ -4,5 +4,5 @@ CREATE SCHEMA staging;
 
 -- source: https://duckdb.org/docs/data/parquet/overview.html
 CREATE TABLE staging.yellow_tripdata AS
-SELECT *
-FROM read_parquet('data/*.parquet', filename=TRUE);
+SELECT *, current_timestamp as load_dt
+FROM read_parquet('data/*.parquet', filename=true);
