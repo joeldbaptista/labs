6947a8b248a0806054d2964437d6c221b3588b6c
joel <jdbaptista@pm.me>
2024-07-22 19:25:00 +0100

    add ampersand to run in parallel

--- 001-taxi/00-pull-data.sh
+++ 001-taxi/00-pull-data.sh
@@ -2,6 +2,6 @@
 set -e
 
 while IFS= read -r line; do
-    echo "Pulling: $line"
-    wget -P ./data "$line"
+    echo "Pulling: $line (in the background)"
+    wget -P ./data "$line" &
 done < ./data/data.csv
