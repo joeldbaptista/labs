9411b435dfdd651e1f058fdb7e97cbc7f1e164c7
joel <jdbaptista@pm.me>
2024-07-22 17:02:38 +0100

    add stip

--- 001-taxi/src/extract_yellow_taxi_data_links.py
+++ 001-taxi/src/extract_yellow_taxi_data_links.py
@@ -20,7 +20,7 @@ def main():
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, "html.parser")
     links = soup.find_all("a")
-    for link in sorted([ link.get("href") for link in links if keep_it(link) ]):
+    for link in sorted([ link.get("href").strip() for link in links if keep_it(link) ]):
         print(link)
 
 
