994ae55595bae651de7d5b9ef819678fc9ac5346
joel <jdbaptista@pm.me>
2024-07-22 19:27:58 +0100

    comment redirect output to null

--- 001-taxi/00-pull-data.sh
+++ 001-taxi/00-pull-data.sh
@@ -3,5 +3,6 @@ set -e
 
 while IFS= read -r line; do
     echo "Pulling: $line (in the background)"
-    wget -P ./data "$line" & > /dev/null
+    # redirect to null if you don't want to have the processes writing to stdout
+    wget -P ./data "$line" & # > /dev/null
 done < ./data/data.csv
